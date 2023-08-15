821bf211f075e7cbe78d9a7daac804f5a451342f
Arthur Lopes <arthurlopr12@gmail.com>
2023-08-15 15:43:10 -0300

    feat: initail commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    fontFamily: {
+      sans: ['var(--font-inter)', 'sans-serif'],
+    },
+    extend: {
+      backgroundImage: {
+        'hero-image': "url('/images/hero-bg.png')",
+      },
+      fontFamily: {
+        mono: ['var(--font-plex-mono)', 'monospace'],
+      },
+      boxShadow: {
+        button: '5px 5px 10px 5px rgba(1, 184, 168, 0.25)',
+      },
+    },
+  },
+  plugins: [],
+}
