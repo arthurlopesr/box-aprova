5334e4a0be528f38181cfca346ad671f0abeefa4
Arthur Lopes <arthurlopr12@gmail.com>
2023-09-03 18:10:25 -0300

    feat: about page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { About } from "./page/home/about/About";
 import { HeroSection } from "./page/home/heroSection/HeroSection";
 
 export default async function Home() {
   return (
     <>
       <HeroSection />
+      <About />
     </>
   )
 }
---
+++ app/page/home/about/About.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+
+export function About() {
+  return (
+    <>
+      <div className="container py-16">
+        <h1 className="font-bold text-3xl text-[#01B8A8]">
+          Sobre:
+        </h1>
+        <div className="mt-[60px] flex items-start justify-between flex-col-reverse lg:flex-row gap-6 lg:gap-56">
+          <Image
+            src="/images/about-image.png"
+            width={365}
+            height={365}
+            alt="about-image"
+          />
+
+          <p>
+            O template no Notion dedicado à preparação para o ENEM é uma ferramenta essencial para estudantes que buscam o sucesso nesse exame desafiador. Com ele, você pode criar um plano de estudos personalizado, definir metas claras e monitorar seu progresso em todas as disciplinas. <br className="mt-1"/>
+
+            O template oferece seções específicas para cada matéria, permitindo que você organize recursos, anotações e exercícios de forma eficiente. Além disso, possui um calendário integrado para agendar revisões e simulados. <br className="mt-1"/>
+
+            A funcionalidade de rastreamento de desempenho permite que você avalie seu progresso ao longo do tempo, identificando áreas de foco. Ele também oferece dicas e técnicas de estudo para otimizar sua preparação. <br className="mt-1"/>
+
+            Em resumo, o template no Notion é uma ferramenta valiosa para a preparação do ENEM, simplificando o processo de estudo e aumentando suas chances de aprovação. Comece a usá-lo hoje e esteja mais perto de alcançar seus objetivos acadêmicos. <br className="mt-1"/>
+          </p>
+        </div>
+      </div>
+    </>
+  )
+}
